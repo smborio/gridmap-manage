55cc9617b29f9c9f8fd714b8295395a1219fc50b
smborio <smborio@idems.international>
2024-01-17 20:03:16 -0300

    Started working on the order of characters in the right column

--- src/app/components/right-section/right-section.component.ts
+++ src/app/components/right-section/right-section.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-right-section',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './right-section.component.html',
   styleUrl: './right-section.component.scss'
 })
+
 export class RightSectionComponent {
+  @ViewChild('svg') svg!: ElementRef;
+  cx = 100;
+  cy = 100;
+  dragging = false;
+
+  onMousedown(event: MouseEvent) {
+    this.dragging = true;
+  }
+
+  onMousemove(event: MouseEvent) {
+    if (this.dragging) {
+      const point = this.svg.nativeElement.createSVGPoint();
+      point.x = event.clientX;
+      point.y = event.clientY;
+      const svgPoint = point.matrixTransform(this.svg.nativeElement.getScreenCTM().inverse());
+      this.cx = svgPoint.x;
+      this.cy = svgPoint.y;
+    }
+  }
+
+  onMouseup() {
+    this.dragging = false;
+  }
+
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8'];
 
 }
