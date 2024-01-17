c21185042e67950aaf48febef96f63c4c31354c0
smborio <smborio@idems.international>
2024-01-17 18:35:18 -0300

    Created button to switch from Game Mode to DM Mode (using bootstrap)

--- src/app/components/top-left/top-left.component.ts
+++ src/app/components/top-left/top-left.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DmModeService } from '../../services/dm-mode.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-top-left',
@@ -7,6 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './top-left.component.html',
   styleUrl: './top-left.component.scss'
 })
-export class TopLeftComponent {
+export class TopLeftComponent implements OnInit, OnDestroy {
+  dmMode!: boolean;
+  subscription!: Subscription;
 
+  constructor(private dmModeService: DmModeService) { }
+
+  ngOnInit() {
+    this.subscription = this.dmModeService.getDmMode().subscribe(value => {
+      this.dmMode = value;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  toggleDmMode() {
+    if (!this.dmMode) {
+      const pin = window.prompt('Enter pin');
+      if (pin === '1234') {
+        this.dmModeService.setDmMode(!this.dmMode);
+        console.log(`DM Mode is ${this.dmMode ? 'ON' : 'OFF'}`);
+      } else {
+        alert('Incorrect pin');
+      }
+    } else {
+      this.dmModeService.setDmMode(!this.dmMode);
+      console.log(`DM Mode is ${this.dmMode ? 'ON' : 'OFF'}`);
+    }
+  }
 }
