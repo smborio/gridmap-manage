29741ca70cdb635cdc272feee56463367e94bd9b
smborio <smborio@idems.international>
2024-01-17 19:20:09 -0300

    reverted some changes

--- src/app/components/right-section/right-section.component.ts
+++ src/app/components/right-section/right-section.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -10,9 +9,5 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrl: './right-section.component.scss'
 })
 export class RightSectionComponent {
-  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8'];
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
-  }
 }
